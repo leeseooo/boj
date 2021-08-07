0c3bbc9b7320758af3a507eda242dad67b43d429
Seoyeong Lee <66051416+leeseooo@users.noreply.github.com>
2021-08-07 12:44:48 +0900

    스피카

--- 21316.cpp
+++ 21316.cpp
@@ -11,11 +11,13 @@ int main() {
 	for (int i = 0; i < 12; i++) {
 		cin >> a >> b;
 		v[a].push_back(b);
-		v[b].push_back(a);
+		v[b].push_back(a);//양방향으로 저장
 	}
+	//정점과 연결된 간선의 갯수는 1,2,3개 중 하나이다.
 	for (int i = 1; i <= 12; i++) {
-		if (v[i].size() == 3)three.push_back(i);// 3 4 7 9
+		if (v[i].size() == 3)three.push_back(i);//3개인 것만 저장 (3,4,7,9)
 	}
+	//간선갯수 3개인 별들과 연결된 정점의 간선의 합이 6인 별이 스피카
 	for (int i = 0; i < three.size(); i++) {
 		int sum = 0;
 		int node = three[i];
